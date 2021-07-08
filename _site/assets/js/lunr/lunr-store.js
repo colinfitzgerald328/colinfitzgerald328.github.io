7f0bc4bf8aaa3c4e0b38cc4b84bc91a9c64bd342
colinfitzgerald328 <colinfitzgerald328@gmail.com>
2021-07-08 10:41:17 -0700

    stuff

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,49 @@
+var store = [{
+        "title": "NBA Statistics Analyzed for Stat 20 Final Project",
+        "excerpt":"library(dplyr) library(ggplot2) nba2019=read.csv(\"/Users/colinfitzgerald/Downloads/nba19.csv\") nba2020=read.csv(\"/Users/colinfitzgerald/Downloads/nba20.csv\") Introduction Background NBA players make millions of dollars each year. However, not all players get played the same. For example, during the basketball season of 2013-14, Kobe Bryant of the LA Lakers was paid $30.4 million whereas Dwight Howard earned $20.5 million. The average person may...","categories": ["tutorial"],
+        "tags": [],
+        "url": "/tutorial/nba-statistics-project/",
+        "teaser": null
+      },{
+        "title": "Threshold Development From 2017 to 2020",
+        "excerpt":"Senior Year of High School (Fall 2017 - Spring 2018): Staple Threshold workout: (2 mile, 2 mile, 1 mile) in 10:00, 10:00, 5:00. We also did another version of this workout where I would run the 2 mile efforts at a slightly slower clip (5:10’s), and then run the mile...","categories": ["tutorial"],
+        "tags": [],
+        "url": "/tutorial/threshold-development/",
+        "teaser": null
+      },{
+        "title": "Data Science Analysis of Running Form",
+        "excerpt":"Analyzing the Correlation Between Running Stride Length and Average Pace Per Mile from datascience import * %matplotlib inline import matplotlib.pyplot as plots plots.style.use('fivethirtyeight') import numpy as np Importing the Data The data generating process was as follows: I went on my run. Once I finished my run, I saved it....","categories": ["tutorial","pinned"],
+        "tags": [],
+        "url": "/tutorial/pinned/datasciencerunning/",
+        "teaser": null
+      },{
+        "title": "Implementing Black Scholes Using Python",
+        "excerpt":"Implementing Black Scholes Using Python #importing all of the necessary modules that we are going to use import numpy as np import scipy.stats as si import sympy as sy from sympy.stats import Normal, cdf from sympy import init_printing init_printing() The function below creates our black-scholes option pricer. Function Used to...","categories": ["tutorial"],
+        "tags": [],
+        "url": "/tutorial/blackscholes/",
+        "teaser": null
+      },{
+        "title": "Modeling Social Forces in Microsoft Excel",
+        "excerpt":"Welcome file 1. The time axis t 2. The configuration axis n 3. The initial condition 4. The Temporal evolution 5. Graphing the result Analysis Modeling Social Forces in Microsoft Excel We know that we are heavily influenced by the opinions and decisions of others, but how can we create...","categories": ["tutorial","pinned"],
+        "tags": [],
+        "url": "/tutorial/pinned/socialforces/",
+        "teaser": null
+      },{
+        "title": "Prickly Pear Invitational Split Analysis",
+        "excerpt":"import numpy as np from datascience import * import matplotlib.pyplot as plt %matplotlib inline p3 = Table.read_table(\"p3.csv\") p3 Split Marc Scott (1) Grant Fisher (2) Sean McGorty (3) Joe Klecker (4) Morgan McDonald (5) Evan Jager (6) Woody Kincaid (7) Brian Barazza (8) Kieran Tuntivate (9) Biya Simbassa (10) John...","categories": ["tutorial","pinned"],
+        "tags": [],
+        "url": "/tutorial/pinned/PricklyPearInvitational/",
+        "teaser": null
+      },{
+        "title": "Analyzing the Difference Between 2019 and 2021 Collegiate Outdoor 1500m Data",
+        "excerpt":"Difference Between 2019 and 2021 Outdoor 1500m Times -Updated 426 Difference between 2019 and 2021 Outdoor Track and Field Times - Collegiate Level&#182; Question: Is there a significant difference in the average 1500m time between 2019 and 2021? More specifically, are the 2021 College Outdoor 1500m times faster, on average,...","categories": ["tutorial","pinned"],
+        "tags": [],
+        "url": "/tutorial/pinned/collegiate-running-data/",
+        "teaser": null
+      },{
+        "title": "Collegiate Track and Field Data, Summarized",
+        "excerpt":"Statistical Updates - Collegiate Track and Field By Colin FitzGerald Author’s Note: This page will be updated weekly as new data becomes available. Page last updated: 5/16/2021 at 10:09 P.M. As I watched the times go up on the board, I couldn’t believe my eyes. “That many guys broke 3:40?!”...","categories": ["tutorial","pinned"],
+        "tags": [],
+        "url": "/tutorial/pinned/tfrrs-table-generator/",
+        "teaser": null
+      }]
